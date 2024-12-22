adab7237d96d1fe2424a9a60e2a3c6eb8687da85
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-12-22 16:13:35 +0200

    nto sending pwdHash and salt

--- ObservatoryAPI/DataAccess/Entities/User.cs
+++ ObservatoryAPI/DataAccess/Entities/User.cs
@@ -1,10 +1,14 @@
-﻿namespace ObservatoryAPI.DataAccess.Entities;
+﻿using System.Text.Json.Serialization;
+
+namespace ObservatoryAPI.DataAccess.Entities;
 
 public class User
 {
     public int Id { get; set; }
     public string Username { get; set; }
     public bool IsAdmin { get; set; }
+    [JsonIgnore]
     public byte[] PasswordHash { get; set; }
+    [JsonIgnore]
     public byte[] PasswordSalt { get; set; }
 }

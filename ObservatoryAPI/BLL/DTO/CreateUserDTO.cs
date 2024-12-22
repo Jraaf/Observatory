59d0f2507738e3b48f24717f99ca184336f1425b
Nasibov Rafael <nasibovrafael1@gmail.com>
2024-12-22 16:15:37 +0200

    ожд

--- ObservatoryAPI/BLL/DTO/CreateUserDTO.cs
+++ ObservatoryAPI/BLL/DTO/CreateUserDTO.cs
@@ -3,7 +3,6 @@
 public class CreateUserDTO
 {
     public string Username { get; set; }
-    public string Email { get; set; }
     public string Password { get; set; }
     public bool IsAdmin { get; set; }
 }
